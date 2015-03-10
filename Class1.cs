513ec5f2d29c9cf08afb1bbb269be482a5b3e88a
Andrey Pisarchik <andrey.pisarchik93@gmail.com>
2015-03-10 23:39:16 +0300

    Commit solutions and config files

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Andrey
+{
+public static class Radix
+{
+    public static double Newton(double x0, double eps, double dx)
+    {
+        double x1 = x0;
+        double dif = Math.Round(F(x1) / F(x1 + dx), 5);
+        double alpha = -1.0 / dif;
+        do
+        {
+            x0 = x1;
+            x1 += Math.Round(alpha * F(x0), 4);
+        } while (Math.Abs(x1 - x0) > eps);
+        return x1;
+    }
+
+
+    public static void Main()
+    {
+        const double eps = 0.01;
+        const double dx = 0.00001;
+        const double x0 = 1.0;
+        Console.WriteLine(Newton(x0, eps, dx));
+        Console.ReadKey();
+    }
+}
---
+++ Project1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public static class Radix
+    {
+        public static double Newton(double A, int  n, double eps)
+        {
+            double x = 1;
+            while(true)
+            {
+                double nx = ((n - 1) * x + A /Math.Pow(x, n - 1)) / n;
+               if (Math.Abs(x - nx) < eps)  break; 
+                x = nx;
+                
+            }
+            return (x);
+        }
+
+
+        public static void Main()
+        {
+            const double eps = 0.0001;
+            const double x = 625;
+            const int  n = 2;
+            Console.WriteLine(Newton(x, n, eps));
+            Console.WriteLine(Math.Pow(x,(double)1/n));
+            Console.ReadKey();
+        }
+    }
+}
